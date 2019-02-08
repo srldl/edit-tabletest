0679afb018368c87ba16ba439c30c0267abd16d0
srldl <siri.uldal@gmail.com>
2019-02-08 13:25:00 +0100

    npm test

---
+++ operations.js
@@ -0,0 +1,17 @@
+function add(x,y)  {
+  return x + y;
+}
+
+function subtract(x,y) {
+  return x - y;
+}
+
+function multiply(x,y) {
+  return x * y;
+}
+
+export default {
+   add: add,
+   subtract: subtract,
+   multiply: multiply
+}
